c1f0f43960ebba969cb5d70ae5dd7e532f55815e
VPeti1 <vpeti011@protonmail.com>
2025-02-11 16:31:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Edge repo
+The extra packages for SteamOS Edge
---
+++ buildrepo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e 
+set -o pipefail
+
+package_name="lutris-git opengamepadui-bin bottles lgogdownloader gzdoom"
+repo_name="edge-repo"
+arch="x86_64"
+
+if [ -e "/usr/bin/yay" ]; then
+    echo "Found Yay"
+else
+    echo "Yay is needed to run this script!"
+    exit 1
+fi
+
+for pkg in $package_name; do
+    yay --builddir "$(pwd)" -S "$pkg" --noconfirm
+done
+
+find . -type f -name "*debug*" -delete
+
+mkdir -p "$repo_name"
+mkdir -p "$repo_name"/"$arch"
+
+find . -type f -name "*pkg.tar*" -exec cp {} "$repo_name"/"$arch" \;
+
+cd "$repo_name"
+
+
+cd "$arch"
+
+repo-add "$repo_name".db.tar.gz *.pkg.tar.zst
+
+unlink "$repo_name".db
+unlink "$repo_name".files
+mv "$repo_name".db.tar.gz "$repo_name".db
+mv "$repo_name".files.tar.gz "$repo_name".files
+
+echo "Repo created"
